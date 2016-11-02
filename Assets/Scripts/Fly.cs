12725eb2bfcd8b8373ff0bd17bc07b4819895a92
YiMin_sinotech <tpuaup@gmail.com>
2016-11-02 17:32:35 +0800

    update first person fly

---
+++ Assets/Scripts/Fly.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fly : MonoBehaviour {
+
+	public float speed = 1f;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = Vector3.forward * moveVertical;
+
+		rb.AddForce (movement * speed);
+	}
+}
